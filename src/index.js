c574d11ac85dce33d50a01ec4cf3aaabb458be80
wrufusii <71870239+wrufusii@users.noreply.github.com>
2021-03-09 08:41:49 +0000

    Grid setup, basic code structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import "./styles.css";
+import "/src/styles.css";
+
+const player = (sign) => {
+  this.sign = sign;
+};
+
+const gameBoard = (() => {
+  const board = new Array(9);
+})();
+
+const displayContoller = (() => {
+  //todo
+  const fields = 1;
+})();
+
+const gameContoller = (() => {
+  //todo
+})();
